33c5af26c73c4e091bf26345d9d702cd7d7d81d2
Simon Eason <sie.eason@gmail.com>
2020-12-12 17:22:43 +0800

    added submit and database pages with links between

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ import bottle
 app = bottle.Bottle()
 
 @app.route("/")
+@app.route("/home")
 def home():
 	return bottle.template("home.html")
+
+@app.route("/submit")
+def submit():
+	return bottle.template("submit.html")
+
+@app.route("/database")
+def database():
+	return bottle.template("database.html")
 
 bottle.run(app, debug=True)
