1246695ab0edab2262f83c7bd0e02c19706d1f38
Simon Eason <sie.eason@gmail.com>
2020-12-12 20:58:15 +0800

    added printing data to page and replacing empty fields with N/A

--- main.py
+++ main.py
@@ -9,7 +9,28 @@ def home():
 
 @app.route("/submit")
 def submit():
-	return bottle.template("submit.html")
+	return bottle.template("submit.html", message="")
+
+@app.route("/submit", method="POST")
+def submit_handler():
+	entry_data = {}
+
+	entry_data["url"] = bottle.request.forms.get("url")
+	entry_data["title"] = bottle.request.forms.get("title")
+	entry_data["author"] = bottle.request.forms.get("author")
+	entry_data["rating"] = bottle.request.forms.get("rating")
+	entry_data["warnings"] = bottle.request.forms.get("warnings")
+	entry_data["summary"] = bottle.request.forms.get("summary")
+	entry_data["notes"] = bottle.request.forms.get("notes")
+
+	message = ""
+
+	for i in entry_data.keys():
+		if entry_data[i] is "":
+			entry_data[i] = "N/A"
+		message += entry_data[i] + ", "
+
+	return bottle.template("submit.html", message=message)
 
 @app.route("/database")
 def database():
