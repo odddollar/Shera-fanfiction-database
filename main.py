7ab3abf552c650ccefaf9d5f337d6857d5b291da
Simon Eason <sie.eason@gmail.com>
2020-12-14 18:11:30 +0800

    removed attempt to create database

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = bottle.Bottle()
 if os.environ.get("APP_LOCATION") == "heroku":
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 	conn = psycopg2.connect(DATABASE_URL, sslmode="require")
-	conn.autocommit = True
-	temp_db = conn.cursor()
-	temp_db.execute("CREATE DATABASE fanfictions")
+	#conn.autocommit = True
+	#temp_db = conn.cursor()
+	#temp_db.execute("CREATE DATABASE fanfictions")
 else:
 	conn = psycopg2.connect(database="fanfictions", user="postgres", password="95283", host="localhost", port="5432")
 
