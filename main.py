047310a86ee12a07cde2820bd3e283e9ad0afc2c
Simon Eason <sie.eason@gmail.com>
2021-05-05 11:49:22 +0800

    added ending newline

--- main.py
+++ main.py
@@ -200,3 +200,4 @@ if os.environ.get("APP_LOCATION") == "heroku":
 	bottle.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
 	bottle.run(app, host="0.0.0.0", port=8080, debug=True)
+	
